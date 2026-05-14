b3683ef4ccac9723a25d45a18193694761c1b4d2
23071sh <23071sh@hvhs.school.nz>
2026-05-15 09:52:38 +1200

    15/05

---
+++ fb_setup.js
@@ -0,0 +1,32 @@
+
+const HTML_OUTPUT = document.getElementById("databaseOutput");
+
+/**************************************************************/
+//Sonia's Sal mini project
+/**************************************************************/
+var GLOBAL_user; //Google's user object
+
+//Set up a listener for the login state of the user
+function fb_login(){
+   authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+}
+// Run when the login state of the user changes. 
+function fb_handleLogin(_user){
+    if (_user){
+        console.log("User is logged in")
+        GLOBAL_user = _user; //Save the user details object to a global variable
+ }  else {
+    console.log("user is Not logged in - Starting the popup process")
+    fb_popupLogin();
+ }
+}
+
+//Run the google login popup
+function fb_popupLogin(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        GLOBAL_user = result.user; //Save the user object to a global variable
+        console.log("User has logged in")
+    });
+}
