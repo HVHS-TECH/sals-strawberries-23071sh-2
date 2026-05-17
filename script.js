65886659990d88ccac5cfcb59fb2205b695efa38
23071sh <23071sh@hvhs.school.nz>
2026-05-18 11:04:10 +1200

    18/05

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 
 console.log("Running Sal's Strawberries")
 
+function fb_login(){
+   authenticationListener = firebase.auth().onAuthStateChanged(fb_handleLogin);
+}
+// Run when the login state of the user changes. 
+function fb_handleLogin(_user){
+    if (_user){
+        console.log("User is logged in")
+        GLOBAL_user = _user; //Save the user details object to a global variable
+ }  else {
+    console.log("user is Not logged in - Starting the popup process")
+    fb_popupLogin();
+ }
+}
+//Run the google login popup
+function fb_popupLogin(){
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then((result) => {
+        GLOBAL_user = result.user; //Save the user object to a global variable
+        console.log("User has logged in")
+    });
+}
+
 function writeForm(){
     // Get the form data
     const favoriteFruit = document.getElementById("favoriteFruit").value;
 }
+
+
